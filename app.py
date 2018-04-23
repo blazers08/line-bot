4a262d61dfc96cfccb702bcaef88f7cb28831206
Denny <blazers20736@gmail.com>
2018-04-24 01:03:47 +0800

    key word

--- app.py
+++ app.py
@@ -40,17 +40,17 @@ def callback():
 def handle_message(event):
     key = event.message.text
     if key == 'profile':
-        message = TextMessage(text="Hello Guys, I'm Denny. I'm from Taipei")
-        line_bot_api.reply_message(event.reply_token, message)
+        message1 = TextMessage(text="Hello Guys, I'm Denny. I'm from Taipei")
+        line_bot_api.reply_message(event.reply_token, message1)
     elif key =='涵涵':
         message2 = TextMessage(text="I Love U, Shelby.")
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, message2)
     elif key =='紀念日':
         message3 = TextMessage(text="我們紀念日是1/19唷")
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, message3)
     else:
         message4 = TextMessage(text="Hello world")
-        line_bot_api.reply_message(event.reply_token, message2)
+        line_bot_api.reply_message(event.reply_token, message4)
 
 
 import os
