0f228b8af1c8d4c18af681a9793f6adabd87a379
Denny <blazers20736@gmail.com>
2018-04-24 00:35:08 +0800

    modified

--- app.py
+++ app.py
@@ -38,8 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextMessage(text="Hello World, this is from code")
-    line_bot_api.reply_message(event.reply_token, message)
+    text = event.message.text
+    if text = 'profile':
+        message = TextMessage(text="Hello World, this is from code")
+        line_bot_api.reply_message(event.reply_token, message)
 
 
 import os
