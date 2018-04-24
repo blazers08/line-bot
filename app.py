a90746a6cb09628053b87e35a55543a87ab4ff89
Denny <blazers20736@gmail.com>
2018-04-24 13:39:32 +0800

    modified

--- app.py
+++ app.py
@@ -49,23 +49,23 @@ def callback():
 
     return 'OK'
 
-def apple_news():
-    target_url = 'http://www.appledaily.com.tw/realtimenews/section/new/'
-    head = 'http://www.appledaily.com.tw'
-    print('Start parsing appleNews....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 15:
-            return content
-        if head in data['href']:
-            link = data['href']
-        else:
-            link = head + data['href']
-        content += '{}\n\n'.format(link)
-    return content
+# def apple_news():
+#     target_url = 'http://www.appledaily.com.tw/realtimenews/section/new/'
+#     head = 'http://www.appledaily.com.tw'
+#     print('Start parsing appleNews....')
+#     rs = requests.session()
+#     res = rs.get(target_url, verify=False)
+#     soup = BeautifulSoup(res.text, 'html.parser')
+#     content = ""
+#     for index, data in enumerate(soup.select('.rtddt a'), 0):
+#         if index == 15:
+#             return content
+#         if head in data['href']:
+#             link = data['href']
+#         else:
+#             link = head + data['href']
+#         content += '{}\n\n'.format(link)
+#     return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
